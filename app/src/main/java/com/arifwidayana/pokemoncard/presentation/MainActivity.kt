a71af18f163dd43f108794bf81b8de08af641931
Arif Widayana <arifwidayana@gmail.com>
2023-01-27 21:35:36 +0700

    Initial commit

---
+++ app/src/main/java/com/arifwidayana/pokemoncard/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arifwidayana.pokemoncard.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arifwidayana.pokemoncard.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
