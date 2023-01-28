6e66c053b278bb47fe52f97133328c7083365f23
Arif Widayana <arifwidayana@gmail.com>
2023-01-28 21:26:10 +0700

    setup wrapper every get data

---
+++ app/src/main/java/com/arifwidayana/pokemoncard/common/utils/Constant.kt
@@ -0,0 +1,18 @@
+package com.arifwidayana.pokemoncard.common.utils
+
+object Constant {
+    //Database
+    const val DB_NAME = "pokemon.db"
+
+    //Path URL
+    const val ID_PATH = "id"
+
+    //Query URL
+    const val QUERY_SEARCH = "q"
+    const val PAGE = "page"
+    const val PAGE_SIZE = "pageSize"
+
+    //Index
+    const val DEFAULT_INDEX_PAGE = 1
+    const val NETWORK_PAGE_SIZE = 20
+}
