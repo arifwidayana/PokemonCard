6cbeda710cd622098cb516a5938f66c4a800a416
Arif Widayana <arifwidayana@gmail.com>
2023-01-30 03:46:28 +0700

    add main nav

--- app/src/main/java/com/arifwidayana/pokemoncard/presentation/MainActivity.kt
+++ app/src/main/java/com/arifwidayana/pokemoncard/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.arifwidayana.pokemoncard.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arifwidayana.pokemoncard.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
